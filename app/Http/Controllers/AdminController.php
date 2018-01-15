04f9f226e46d4ea89cfec4733782cdcefa3b79be
Huong <you@example.com>
2018-01-15 21:13:46 +0700

    typelist

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\FoodType;
 
 class AdminController extends Controller
 {
     function getListType(){
-        return view('pages.list-type');
+        $types = FoodType::all();
+        return view('pages.list-type',compact('types'));
     }
 }
--- resources/views/pages/list-type.blade.php
+++ resources/views/pages/list-type.blade.php
@@ -1,15 +1,39 @@
-@extends('layout')
-@section('title',"Danh sách loại")
-
-@section('content')
+@extends('layout') @section('title',"Danh sách loại") @section('content')
 <section class="wrapper">
     <div class="panel panel-body">
         <section class="content">
             <div class="panel panel-default">
-                <div class="panel-heading"><b>Admin</b>
+                <div class="panel-heading">
+                    <b>Danh sách loại</b>
                 </div>
                 <div class="panel-body">
-                    content
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Tên loại</th>
+                                <th>Mô tả</th>
+                                <th>Hình</th>
+                                <th>Tuỳ chọn</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $stt = 1;?>
+                            @foreach($types as $t)
+                            <tr>
+                                <td>{{$stt++}}</td>
+                                <td>{{$t->name}}</td>
+                                <td>{{$t->description}}</td>
+                                <td>
+                                    <img src="admin/img/hinh_loai_mon_an/{{$t->image}}" style="height:80px">
+                                </td>
+                                <td>
+                                    Edit | Delete
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </section>
