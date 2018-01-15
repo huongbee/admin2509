d7a057837da0849f1482ec3b7f67478bc4f48516
Huong <you@example.com>
2018-01-15 20:11:35 +0700

    layout

---
+++ resources/views/pages/list-type.blade.php
@@ -0,0 +1,13 @@
+<section class="wrapper">
+    <div class="panel panel-body">
+        <section class="content">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Admin</b>
+                </div>
+                <div class="panel-body">
+                    content
+                </div>
+            </div>
+        </section>
+    </div>
+    </section>
